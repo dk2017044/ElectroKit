b38f327cc0714684a7428f1da3438256becbdbfe
Dilip Kumar <dk2017044@hotmail.com>
2026-07-24 11:15:22 +0530

    Fix release signing config to use release.keystore for v3.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
